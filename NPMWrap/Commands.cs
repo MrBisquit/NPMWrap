095ff45e17bc2e15d1de3ecf4c3f711c210c794c
MrBisquit <william@wtdawson.info>
2023-11-07 18:37:29 +0000

    Add project files.

---
+++ NPMWrap/Commands.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPMWrap
+{
+    public static class Commands
+    {
+        public class Config
+        {
+            public bool UseYarn { get; set; } = false;
+            public string Directory { get; set; } = null;
+            public bool IsDebug { get; set; } = false;
+            public bool WaitForExit { get; set; } = false;
+        }
+
+        public static void RunBaseInstall(Config Configuration)
+        {
+            if(Configuration.Directory == null)
+            {
+                throw new Exception("NPMWrap: Configuration invalid, Directory was null.")
+                {
+                    HResult = 0, // Temp
+                    HelpLink = string.Empty // Temp
+                };
+            }
+
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Configuration.UseYarn ? "yarn" : "npm",
+                    Arguments = Configuration.UseYarn ? "" : "install",
+                    CreateNoWindow = !Configuration.IsDebug,
+                    WindowStyle = Configuration.IsDebug ? ProcessWindowStyle.Normal : ProcessWindowStyle.Hidden,
+                    WorkingDirectory = Configuration.Directory,
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+
+            if(Configuration.WaitForExit)
+            {
+                process.WaitForExit();
+            }
+        }
+
+        public static void RunInstall(string PackageName, Config Configuration)
+        {
+            if (Configuration.Directory == null)
+            {
+                throw new Exception("NPMWrap: Configuration invalid, Directory was null.")
+                {
+                    HResult = 0, // Temp
+                    HelpLink = string.Empty // Temp
+                };
+            }
+
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Configuration.UseYarn ? "yarn" : "npm",
+                    Arguments = Configuration.UseYarn ? $"add {PackageName}" : $"install {PackageName}",
+                    CreateNoWindow = !Configuration.IsDebug,
+                    WindowStyle = Configuration.IsDebug ? ProcessWindowStyle.Normal : ProcessWindowStyle.Hidden,
+                    WorkingDirectory = Configuration.Directory,
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+
+            if (Configuration.WaitForExit)
+            {
+                process.WaitForExit();
+            }
+        }
+
+        public static void RunInstall(string PackageName, string PackageVersion, Config Configuration)
+        {
+            if (Configuration.Directory == null)
+            {
+                throw new Exception("NPMWrap: Configuration invalid, Directory was null.")
+                {
+                    HResult = 0, // Temp
+                    HelpLink = string.Empty // Temp
+                };
+            }
+
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Configuration.UseYarn ? "yarn" : "npm",
+                    Arguments = Configuration.UseYarn ? $"add {PackageName}@{PackageVersion}" : $"install {PackageName}@{PackageVersion}",
+                    CreateNoWindow = !Configuration.IsDebug,
+                    WindowStyle = Configuration.IsDebug ? ProcessWindowStyle.Normal : ProcessWindowStyle.Hidden,
+                    WorkingDirectory = Configuration.Directory,
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+
+            if (Configuration.WaitForExit)
+            {
+                process.WaitForExit();
+            }
+        }
+
+        public static void RunUninstall(string PackageName, Config Configuration)
+        {
+            if (Configuration.Directory == null)
+            {
+                throw new Exception("NPMWrap: Configuration invalid, Directory was null.")
+                {
+                    HResult = 0, // Temp
+                    HelpLink = string.Empty // Temp
+                };
+            }
+
+            Process process = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = Configuration.UseYarn ? "yarn" : "npm",
+                    Arguments = Configuration.UseYarn ? $"remove {PackageName}" : $"uninstall {PackageName}",
+                    CreateNoWindow = !Configuration.IsDebug,
+                    WindowStyle = Configuration.IsDebug ? ProcessWindowStyle.Normal : ProcessWindowStyle.Hidden,
+                    WorkingDirectory = Configuration.Directory,
+                    UseShellExecute = true
+                }
+            };
+
+            process.Start();
+
+            if (Configuration.WaitForExit)
+            {
+                process.WaitForExit();
+            }
+        }
+    }
+}
---
+++ NPMWrap/Registry.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using NPMWrap.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace NPMWrap
+{
+    public static class Registry
+    {
+        public class Config
+        {
+            public string UserAgent { get; set; } = "NPMWrap NPM API Client for C#";
+        }
+
+        public static async Task<SearchResult> SearchPackagesAsync(string SearchTerm, Config Configuration)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(Configuration.UserAgent);
+                var response = await client.GetAsync($"https://registry.npmjs.org/-/v1/search?text={SearchTerm}");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    SearchResult PackagesData = JsonConvert.DeserializeObject<SearchResult>(content);
+                    return PackagesData;
+                }
+                else
+                {
+                    throw new Exception("NPMWrap: Failed to fetch packages from the NPM registry, received a not OK response status code.")
+                    {
+                        HResult = 0, // Temp
+                        HelpLink = string.Empty // Temp
+                    };
+                }
+            }
+        }
+
+        public static async Task<RegistryPackageVersion> GetPackageVersion(string Package, Config Configuration)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(Configuration.UserAgent);
+                var response = await client.GetAsync($"https://registry.npmjs.org/{Package}/latest");
+                if(response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    RegistryPackageVersion PackageData = JsonConvert.DeserializeObject<RegistryPackageVersion>(content);
+                    return PackageData;
+                } else
+                {
+                    throw new Exception("NPMWrap: Failed to fetch package data from the NPM registry, received a not OK response status code.")
+                    {
+                        HResult = 0, // Temp
+                        HelpLink = string.Empty // Temp
+                    };
+                }
+            }
+        }
+
+        public static async Task<RegistryPackageVersion> GetPackageVersion(string Package, string Version, Config Configuration)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(Configuration.UserAgent);
+                var response = await client.GetAsync($"https://registry.npmjs.org/{Package}/{Version}");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    RegistryPackageVersion PackageData = JsonConvert.DeserializeObject<RegistryPackageVersion>(content);
+                    return PackageData;
+                }
+                else
+                {
+                    throw new Exception("NPMWrap: Failed to fetch package data from the NPM registry, received a not OK response status code.")
+                    {
+                        HResult = 0, // Temp
+                        HelpLink = string.Empty // Temp
+                    };
+                }
+            }
+        }
+    }
+}
